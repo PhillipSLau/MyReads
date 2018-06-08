08cdca4c78d608f56667286fc934cc3b4521ced3
PhillipSLau <35332854+PhillipSLau@users.noreply.github.com>
2018-06-08 16:31:46 -0700

    File Upload
    Files uploaded from Udacity MyReads Project

---
+++ src/BookShelf.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+class BookShelf extends React.Component {
+	
+	render(){
+		
+		return(
+		  <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Currently Reading</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      
+                    </ol>
+                  </div>
+                </div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Want to Read</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                     
+                    </ol>
+                  </div>
+                </div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Read</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      
+                    </ol>
+                  </div>
+                </div>
+              </div>
+			  <div className="open-search">
+				<Link to='/Search'>Add a Book</Link>
+            </div>
+			</div>
+		)
+	}
+}
+
+export default BookShelf
